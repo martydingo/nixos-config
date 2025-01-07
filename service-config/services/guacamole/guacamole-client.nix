02825e09efdaf86c7a4d5701274ce8cd97271b59
Martin George <marty@dingo.foo>
2025-01-07 13:51:44 +0000

    commit

---
+++ service-config/services/guacamole/guacamole-client.nix
@@ -0,0 +1,11 @@
+{ ... }:
+{
+  services.guacamole-client = {
+    enable = true;
+    enableWebserver = true;
+    settings = {
+      guacd-port = 4822;
+      guacd-hostname = "localhost";
+    };
+  };
+}
