2fd17bb0fd7051a01e138e7ed57cabc545b51447
Martin George <marty@dingo.foo>
2025-01-11 16:38:13 +0000

    commit

--- service-config/services/squeezelite.nix
+++ service-config/services/squeezelite.nix
@@ -4,5 +4,5 @@
     enable = true;
     extraArguments = ''-o pipewire -s 192.168.254.6 -n "Office PC"'';
   };
-  systemd.services.squeezelite.serviceConfig.supplementaryGroups = "pipewire";
+  systemd.services.squeezelite.serviceConfig.DynamicUser = "false";
 }
