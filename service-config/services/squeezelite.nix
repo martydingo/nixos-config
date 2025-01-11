e1d7792845f4c845d8386bfa9cdfe5173aae59ef
Martin George <marty@dingo.foo>
2025-01-11 16:35:05 +0000

    commit

--- service-config/services/squeezelite.nix
+++ service-config/services/squeezelite.nix
@@ -5,4 +5,5 @@
     extraArguments = ''-o pipewire -s 192.168.254.6 -n "Office PC"'';
   };
   systemd.services.squeezelite.serviceConfig.supplementaryGroups = "pipewire";
+  systemd.services.squeezelite.serviceConfig.Environment = [ "XDG_RUNTIME_DIR=/run/user/1000" ];
 }
