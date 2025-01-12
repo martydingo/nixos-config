5955c370975c67e2450321fdefc94a1b71a9cbfe
Martin George <marty@dingo.foo>
2025-01-12 15:04:39 +0000

    commit

--- hardware-config/hardware/audio.nix
+++ hardware-config/hardware/audio.nix
@@ -3,7 +3,6 @@
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
-    wireplumber.enable = true;
     pulse.enable = true;
     alsa.enable = true;
     systemWide = true;
@@ -14,30 +13,6 @@
             "default.clock.rate" = 192000;
             "audio.format" = "S32LE";
           };
-        };
-        "10-network-audio-output" = {
-          "context.modules" = [{
-            "name" = "libpipewire-module-protocol-simple";
-            "args" = {
-              # Provide sink
-              "capture" = true;
-              "audio.rate" = 96000;
-              "audio.format" = "S24_32";
-              "audio.channels" = 2;
-              "audio.position" = [ "FL" "FR" ];
-
-              # The addresses this server listens on for new
-              # client connections
-              "server.address" = [
-                "tcp:4711"
-              ];
-
-              "capture.props" = {
-                # Make this a sink instead of a capture stream
-                "media.class" = "Audio/Sink";
-              };
-            };
-          }];
         };
       };
     };
