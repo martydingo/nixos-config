d34389635f5f24ada9bb0e7adf4fbc7a2fd57546
Martin George <marty@dingo.foo>
2025-02-11 18:16:39 +0000

    commit

--- program-config/programs/regreet.nix
+++ program-config/programs/regreet.nix
@@ -1,20 +1,20 @@
-{ inputs, pkgs, ... }:
-{
+{ inputs, pkgs, ... }: {
   programs.regreet = {
     enable = true;
     package = pkgs.greetd.regreet;
     theme = {
       name = "Gruvbox-Material-Dark";
-      package = inputs.nixpkgs-unstable.outputs.legacyPackages.${pkgs.stdenv.hostPlatform.system}.pkgs.gruvbox-material-gtk-theme;
+      package =
+        inputs.nixpkgs-unstable.outputs.legacyPackages.${pkgs.stdenv.hostPlatform.system}.pkgs.gruvbox-material-gtk-theme;
     };
-    cursorTheme = {
-      name = "Capitaine Cursors (Gruvbox) 32";
-      package = inputs.nixpkgs-unstable.outputs.legacyPackages.${pkgs.stdenv.hostPlatform.system}.capitaine-cursors-themed;
-    };
-    iconTheme = {
-      name = "Gruvbox-Plus-Dark";
-      package = inputs.nixpkgs-unstable.outputs.legacyPackages.${pkgs.stdenv.hostPlatform.system}.gruvbox-plus-icons;
-    };
+    # cursorTheme = {
+    #   name = "Capitaine Cursors (Gruvbox) 32";
+    #   package = inputs.nixpkgs-unstable.outputs.legacyPackages.${pkgs.stdenv.hostPlatform.system}.capitaine-cursors-themed;
+    # };
+    # iconTheme = {
+    #   name = "Gruvbox-Plus-Dark";
+    #   package = inputs.nixpkgs-unstable.outputs.legacyPackages.${pkgs.stdenv.hostPlatform.system}.gruvbox-plus-icons;
+    # };
     settings = {
       background.path = "/var/5120x1440wallpaper_51202149947_o.png";
       env = {
--- service-config/services/printer.nix
+++ service-config/services/printer.nix
@@ -4,13 +4,14 @@
     nssmdns4 = true;
   };
   services.printing.drivers = [ pkgs.ptouch-driver ];
+  services.printing.enable = true;
   hardware.printers = {
     ensureDefaultPrinter = "Brother_QL-810W";
     ensurePrinters = [{
       deviceUri = "socket://192.168.254.107";
       location = "Home";
-      name = "Brother";
-      model = "QL-810W";
+      name = "Brother_QL-810W";
+      model = "ptouch-driver/Brother-QL-810W-ptouch-ql.ppd.gz";
     }];
   };
 }
